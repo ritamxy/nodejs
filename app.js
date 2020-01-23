974805a85506f7346501567b990cc40e45d2d6e4
ritamxy <ritamxy@gmail.com>
2020-01-23 21:30:37 +1100

    add change

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require("express");
 var app = express();
+
+function requestComming(req, res, next) {
+  console.log("master");
+  next();
+}
+function authCheck(req, res, next) {
+  if (req.params.user.include("Alex")) {
+    res.send("out");
+  }
+  next();
+}
+
+app.get("/:user", requestComming, authCheck, function(req, res) {
+  res.send("Hello JR");
+});
+
 app.get("/", function(req, res) {
   res.send("Hello JR");
 });
 
-app.listen(8888);
+app.listen(8888, () => {
+  console.log("server is run on 8888");
+});
